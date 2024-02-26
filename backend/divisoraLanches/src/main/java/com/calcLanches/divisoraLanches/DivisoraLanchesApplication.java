f2c79d9ea183a21673908ce5065f8718c30e9833
fogolari <enzkf@outlook.com>
2024-02-26 19:14:59 -0300

    initial commit

---
+++ backend/divisoraLanches/src/main/java/com/calcLanches/divisoraLanches/DivisoraLanchesApplication.java
@@ -0,0 +1,13 @@
+package com.calcLanches.divisoraLanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisoraLanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivisoraLanchesApplication.class, args);
+	}
+
+}
---
+++ backend/divisoraLanches/src/test/java/com/calcLanches/divisoraLanches/DivisoraLanchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcLanches.divisoraLanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisoraLanchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
